652b909b434bee974f917bb9e9429e09f677ab66
Agrowmart123 <info@agrowmartindia.com>
2026-03-12 15:39:35 +0530

    Update apiClient.jsbase url

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -5,7 +5,7 @@
 // import { getAuthToken, logout, refreshAccessToken } from "./authService";
 
 // const api = axios.create({
-//   baseURL: "http://localhost:8080/api",
+//   baseURL: "https://api.agrowmartindia.com",
 //   headers: {
 //     "Content-Type": "application/json",
 //   },
